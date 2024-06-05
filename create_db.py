be4c246a1b8c4c50d3dbbc56deaefe839149703a
unknown <jvoliveira2005@gmail.com>
2024-06-04 21:40:45 -0300

    update

--- create_db.py
+++ create_db.py
@@ -4,36 +4,38 @@ mydb = mysql.connector.connect(
   host="localhost",
   user="root",
   password="",
-  database="notes"
+  database="supermercado"
 )
 
 mycursor = mydb.cursor()
 
-# mycursor.execute("""CREATE TABLE Users (
-#   id INT AUTO_INCREMENT PRIMARY KEY,
-#   username VARCHAR(20) UNIQUE NOT NULL,
-#   password VARCHAR(200) NOT NULL,
-# );
-# """)
+mycursor.execute("""CREATE TABLE users (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  username VARCHAR(20) UNIQUE NOT NULL,
+  password VARCHAR(200) NOT NULL
+);
+""")
 
-# mycursor.fetchall()
+mycursor.execute("""CREATE TABLE produtos (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  descricao VARCHAR(75),
+  qtd INT,
+  preco DOUBLE
+);
+""")
 
-# mycursor.execute("""CREATE TABLE produtos (
-#   id INT AUTO_INCREMENT PRIMARY KEY,
-#   descricao VARCHAR(75),
-#   qtd INT,
-#   preco DOUBLE
-# );
-# """)
+mycursor.execute("""INSERT INTO produtos (descricao, qtd, preco) VALUES
+('Arroz', 10, 19.99),
+('Frango', 20, 29.99),
+('Batata', 15, 14.99),
+('Corinthians', 5, 49.99),
+('Ovo', 8, 9.99);
+""")
 
-# mycursor.fetchall()
+mydb.commit()
 
-# INSERT INTO produtos (descricao, qtd, preco) VALUES
-# ('Arroz', 10, 19.99),
-# ('Frango', 20, 29.99),
-# ('Batata', 15, 14.99),
-# ('Corinthians', 5, 49.99),
-# ('Ovo', 8, 9.99);
+mycursor.execute("""INSERT INTO Users (username, password) 
+VALUES ('ademilson', 'senha123')
+""")
 
-# INSERT INTO Users (username, password) 
-# VALUES ('ademilson', 'senha123');
+mydb.commit()
